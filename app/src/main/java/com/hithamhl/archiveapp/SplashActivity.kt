14056ee6d2b5b0ccd210d4e3c521ddc163e67b2b
hitham husham <hitham.apps4@gmail.com>
2020-10-25 18:59:21 +0300

    splash add timer

--- app/src/main/java/com/hithamhl/archiveapp/SplashActivity.kt
+++ app/src/main/java/com/hithamhl/archiveapp/SplashActivity.kt
@@ -3,6 +3,8 @@ package com.hithamhl.archiveapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_splash.*
+import java.util.*
 
 class SplashActivity : AppCompatActivity() {
 
@@ -10,8 +12,19 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        startActivity(Intent(this,MainActivity::class.java))
-        finish()
+        splashIcon_iv.run {
+            animate().scaleX(2F).scaleY(2f).setDuration(1000).withEndAction {
+                splashIcon_iv.animate().scaleX(1f).scaleY(1f).duration = 500 }
+        }
+
+        Timer().schedule(object :TimerTask(){
+            override fun run() {
+                startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+                finish()
+            }
+
+        },2000L)
+
 
 
 
