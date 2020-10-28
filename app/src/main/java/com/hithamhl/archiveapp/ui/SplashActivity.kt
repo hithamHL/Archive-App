7e5cb0af592c6a4938788f2c10ddd420a53f46d3
hitham husham <hitham.apps4@gmail.com>
2020-10-28 18:49:36 +0300

    add fragment

--- app/src/main/java/com/hithamhl/archiveapp/ui/SplashActivity.kt
+++ app/src/main/java/com/hithamhl/archiveapp/ui/SplashActivity.kt
@@ -1,8 +1,10 @@
-package com.hithamhl.archiveapp
+package com.hithamhl.archiveapp.ui
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hithamhl.archiveapp.MainActivity
+import com.hithamhl.archiveapp.R
 import kotlinx.android.synthetic.main.activity_splash.*
 import java.util.*
 
@@ -12,21 +14,27 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        splashIcon_iv.run {
-            animate().scaleX(2F).scaleY(2f).setDuration(1000).withEndAction {
-                splashIcon_iv.animate().scaleX(1f).scaleY(1f).duration = 500 }
-        }
+      this.startScreen()
+    }
+
+
+    private fun startScreen(){
+
+        runAnimation()
 
         Timer().schedule(object :TimerTask(){
             override fun run() {
-                startActivity(Intent(this@SplashActivity,MainActivity::class.java))
+                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
                 finish()
             }
 
         },2000L)
+    }
 
-
-
-
+    private fun runAnimation(){
+        splashIcon_iv.run {
+            animate().scaleX(2F).scaleY(2f).setDuration(1000).withEndAction {
+                splashIcon_iv.animate().scaleX(1f).scaleY(1f).duration = 500 }
+        }
     }
 }
